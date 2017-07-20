b5b8e4b87ba047778bee68401d884da07a9c7186
ornab <hzamil17@gmail.com>
2017-07-20 23:53:06 +0600

    Initial Commit

---
+++ app/Contact.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    
+    protected $fillable=[
+        
+        'name','company', 'email', 'address', 'phone', 'group_id','photo' 
+        
+    ];
+    
+    public function group(){
+    
+    return $this->belongsTo('App\Group');
+        
+    }
+}
